f6e1d12f8c128bb18cbab9c0a6cd4073730bc7c2
oregami <oregami@163.com>
2016-08-05 15:16:20 +0800

    add function exe by name

--- inject.js
+++ inject.js
@@ -2,4 +2,19 @@
  * Created by oregami on 16/8/5.
  */
 
-alert($('body').find('.hero-content, .text-left').html());
+//alert($('body').find('.hero-content, .text-left').html());
+injectTools = {
+    "alert": function () {
+        alert("inject alert");
+    },
+    "function1": function() {
+        console.log('function1');
+    },
+    "function2": function() {
+        console.log('function2');
+    }
+};
+
+exeFunctionByName = function exeFunctionByName(funcName) {
+    injectTools[funcName]();
+}
